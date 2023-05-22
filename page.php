b222829001afb23806b93c399d8e7003b12a7f2b
Nicholas <ntravaglini@icloud.com>
2023-05-21 23:54:23 -0400

    parent and child page customization

--- page.php
+++ page.php
@@ -19,6 +19,7 @@ while(have_posts()) {
 
     <?php
         $theParent = wp_get_post_parent_id(get_the_ID());
+        // Equals ID of current page's parent page or if current page is parent it will equal 0
         if ($theParent) { ?>
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
@@ -28,13 +29,31 @@ while(have_posts()) {
        <?php }
     ?>
 
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php
+    $testArray = get_pages(array(
+        'child_of' => get_the_ID() 
+    ));
+
+    if ($theParent or $testArray) { ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php 
+            if ($theParent) {
+                $findChildrenOf = $theParent;
+            } else {
+                $findChildrenOf = get_the_ID();
+            }
+
+            wp_list_pages(array(
+                'title_li' => NULL,
+                'child_of' => $findChildrenOf,
+                'sort_column' => 'menu_order'
+            ));
+          ?>
         </ul>
-      </div> -->
+      </div>
+    <?php } ?>
 
       <div class="generic-content">
         <?php the_content(); ?>
