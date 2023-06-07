d4547e823c14830766488d796b5f331e8a853e1d
Nicholas <ntravaglini@icloud.com>
2023-06-06 22:49:54 -0400

    Archive pages

--- archive-program.php
+++ archive-program.php
@@ -5,38 +5,24 @@ get_header(); ?>
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>;)"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">All Events</h1>
+    <h1 class="page-banner__title">All Programs</h1>
         
     <div class="page-banner__intro">
-      <p>See what is going on in our world</p>
+      <p>There is something for everyone.</p>
     </div>
   </div>
 </div>
 
 <div class="container container--narrow page-section">
+<ul class="link-list min-list">
   <?php
     while(have_posts()) {
       the_post(); ?>
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-          <span class="event-summary__month"><?php 
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M');
-          ?></span>
-          <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
-        </a>
-      <div class="event-summary__content">
-        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-        <p><?php echo wp_trim_words(get_the_content(), 18); ?><br> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-      </div>
-      </div>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title();?></a></li>
    <?php }
     echo paginate_links();
   ?>
-
-      <hr class="section-break">
-
-      <p>Looking for a recap of past events? <a href="/past-events">Check out our past events achive</a></p>
+</ul>
 
 </div>
 
