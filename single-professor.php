f5b6401b724826d639cb7a4c71cf974cfd57084c
Nicholas <ntravaglini@icloud.com>
2023-06-11 21:52:04 -0400

    register image sizes

--- single-professor.php
+++ single-professor.php
@@ -18,7 +18,17 @@ while(have_posts()) {
     <div class="container container--narrow page-section">
 
         <div class="generic-content">
-            <?php the_content(); ;?>
+            <div class="row group">
+
+              <div class="one-third">
+                <?php the_post_thumbnail('professor-portrait'); ?>
+              </div>
+
+              <div class="two-thirds">
+                <?php the_content(); ?>
+              </div>
+
+            </div>
         </div>
 
         <?php 
--- single-program.php
+++ single-program.php
@@ -46,10 +46,20 @@ while(have_posts()) {
               echo '<hr class="section-break">';
               echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors  </h2>';
 
+            echo '<ul class="professor-cards">';  
             while($relatedProfessors->have_posts()) {
               $relatedProfessors->the_post(); ?>
-                <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                <li class="professor-card__list-item">
+                  <a class="professor-card" href="<?php the_permalink(); ?>">
+                    <img src="<?php the_post_thumbnail_url('professor-landscape') ?>" alt="" class="professor-card__image">
+                      <span class="professor-card__name">
+                        <?php the_title(); ?>
+                      </span>
+                    </img>
+                  </a>
+                </li>
             <?php }
+            echo '</ul>';
             }
 
             wp_reset_postdata();
